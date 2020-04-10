8c388a087501c12eb221a4f445fe3ac3eec05072
Brice Wilson <brice@5wilsons.net>
2020-04-10 16:05:43 -0400

    upgraded to Angular 9

--- booktracker/server.js
+++ booktracker/server.js
@@ -2,25 +2,18 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./server/routes/index');
 var readers = require('./server/routes/readers');
 var books = require('./server/routes/books');
 var errors = require('./server/routes/errors');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, '/server/views'));
-app.set('view engine', 'jade');
-
 app.use(favicon(path.join(__dirname, 'dist/booktracker/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist/booktracker')));
 
 // app.use('/', routes);
@@ -61,8 +54,6 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
-var debug = require('debug')('server');
 
 app.set('port', process.env.PORT || 3000);
 
