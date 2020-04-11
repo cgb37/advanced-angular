679239187d710525b2ab575b000ee8c1e57cf30b
Brice Wilson <brice@5wilsons.net>
2020-04-11 17:14:45 -0400

    added code for feature module demo (TAG: feature_module_demo_complete)

--- booktracker/src/app/app.module.ts
+++ booktracker/src/app/app.module.ts
@@ -3,28 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AddBookComponent } from './add-book/add-book.component';
 import { AddReaderComponent } from './add-reader/add-reader.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { EditBookComponent } from './edit-book/edit-book.component';
 import { EditReaderComponent } from './edit-reader/edit-reader.component';
+import { BooksModule } from './books/books.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    AddBookComponent,
     EditReaderComponent,
-    EditBookComponent,
     AddReaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BooksModule
   ],
   bootstrap: [AppComponent]
 })
